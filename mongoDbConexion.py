5926fedc85c83691dbee50e79330bae0cc3811d2
dcastan93 <dcastan93@gmail.com>
2022-05-17 18:13:07 -0500

    Este archivo contiene la información mñas actualpara acceder a la cuenta de mongoDb desde python

--- mongoDbConexion.py
+++ mongoDbConexion.py
@@ -1,4 +1,4 @@
 import pymongo
 import dns # required for connecting with SRV
-client = pymongo.MongoClient("mongodb+srv://dcastan93:myRealPassword@cluster0.mongodb.net/test?w=majority")
+client = pymongo.MongoClient("mongodb+srv://dcastan93:<password>@cluster0.w7eam.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
